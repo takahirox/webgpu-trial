a6934c465b8b23daf09a0920852a0a22b7c27c4a
Takahiro <hogehoge@gachapin.jp>
2019-12-18 13:11:39 +0900

    Clean up js/glmatrix.js and js/glslang.js

--- js/glslang.js
+++ js/glslang.js
@@ -1,4 +1,5 @@
-export default async function() {
-  const glslModule = await import('https://unpkg.com/@webgpu/glslang@0.0.12/dist/web-devel/glslang.js');
-  return await glslModule.default();
-}
+import glslangModule from 'https://unpkg.com/@webgpu/glslang@0.0.12/dist/web-devel/glslang.js';
+
+export {
+  glslangModule
+};
